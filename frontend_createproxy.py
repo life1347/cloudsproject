4075ae3054df9f44df74bdfedd115b4f7976b0af
03067 <cwc_03067@live.com>
2014-01-06 22:31:29 +0800

    Update frontend_createproxy (1/6/2014)
    曾加判斷一個tenant裡面是否有存在floating ip 而沒有被assign 到VM
    有的話就用該floating ip

--- frontend_createproxy.py
+++ frontend_createproxy.py
@@ -3,6 +3,7 @@ import logging
 import pprint
 
 import time
+
 import os
 
 from time import sleep, asctime
@@ -31,7 +32,10 @@ KEYSTONE_URL='https://openstack.nctu.edu.tw/keystone/v2.0'
 
 
 
+
+
 def create_proxy(now):
+
 
 
 	username = ['0256547','0256081','0256017','0156153']
@@ -53,63 +57,111 @@ def create_proxy(now):
 	nc=nclient.Client(auth_url=KEYSTONE_URL, username = username[now], api_key = password[now], project_id=tenant_name[now],service_type='compute', insecure = True)
 
 
+
 	router_name = username[now] + '_router'
+
 	network_name = username[now] + '_network'
+
 	subnet_name = username[now] + '_subnet'
+
 	mynet_id = ''
+
 	sub_id = ''
+
 	router1_id = ''
+
 	if not ne.list_networks(name = network_name)['networks']:
+
 		print 'create network'
+
 		mynet = ne.create_network({'network':{'name': network_name, 'admin_state_up': True, }})
+
 		mynet_id = mynet['network']['id']
+
 	else:
+
 		mynet = ne.list_networks(name = network_name)
+
 		mynet_id = mynet['networks'][0]['id']
+
 		print 'mynet_id=',
+
 		print mynet_id
 
+
+
 	if not ne.list_subnets(name = subnet_name)['subnets']:
+
 		print 'create subnet'
+
 		cidr = 250
+
 		subnet_cidr = '192.168.' + str(cidr+now) + '.0/24'
+
 		sub = ne.create_subnet({'subnet':{'name':subnet_name, 'network_id': mynet_id, 'ip_version': 4, 'cidr': subnet_cidr, 'dns_nameservers': ['8.8.8.8']}})
+
 		sub_id = sub['subnet']['id']
+
 	else:
+
 		sub = ne.list_subnets(name = subnet_name)
+
 		sub_id = sub['subnets'][0]['id']
+
 		print 'sub_id=',
+
 		print sub_id
 
+
+
 	if not ne.list_routers(name = router_name)['routers']:
+
 		print 'create router'
+
 		ne.create_router({'router':{ 'name': router_name, 'admin_state_up' : True }})
+
 		router1 = ne.list_routers(name = router_name)
+
 		router1_id = router1['routers'][0]['id']
+
 		ext_net = ne.list_networks(name = 'ext-net') 
+
 		ext_net_id = ext_net['networks'][0]['id']
+
 		ne.add_gateway_router(router1_id,{'network_id': ext_net_id})
+
 		ne.add_interface_router(router1_id,{'subnet_id': sub_id})
+
 	#else:
+
 	#	print 'set gateway and interface'
+
 	#	router1 = ne.list_routers(name = router_name)
+
 	#	router1_id = router1['routers'][0]['id']
+
 	#	ext_net = ne.list_networks(name = 'ext-net') 
+
 	#	ext_net_id = ext_net['networks'][0]['id']
+
 	#	ne.add_gateway_router(router1_id,{'network_id': ext_net_id})
+
 	#	ne.add_interface_router(router1_id,{'subnet_id': sub_id})
 
+
+
 	if not nc.keypairs.findall(name="0256547_key"):
+
 	    with open(os.path.expanduser('~/.ssh/id_rsa.pub')) as fpubkey:
+
 		nc.keypairs.create(name="0256547_key", public_key=fpubkey.read())
+
 
 
 	image = nc.images.find(name = "0156153_ubuntu")
 
 	flavor = nc.flavors.find(name = "cpu4ram4disk20")
-
-	floating_ip = nc.floating_ips.create("ext-net")
-
+        
 
       
 
@@ -132,23 +184,48 @@ def create_proxy(now):
 
 
 	status = server1.status
+
 	while status == 'BUILD':
+
 		    time.sleep(5)
+
 		    # Retrieve the instance again so the status field updates
+
 		    server1 = nc.servers.get(server1.id)
+
 		    status = server1.status
+
 	print "status: %s" % status
 
-
-	server1.add_floating_ip(floating_ip)
-
-
-
-
-
+	
 	f = open('iplist_front.txt', 'a')
+	#check floating ip to avoid unnecessary  create floating ip
 
-	f.write(floating_ip.ip + '\n')
+	floatingiplist =  nc.floating_ips.list()
+	
+	a = 0
+	for x in range(len(floatingiplist)) : 
+		if floatingiplist[x].instance_id == None and floatingiplist[x].ip != None:
+			server1.add_floating_ip(floatingiplist[x].ip)
+			f.write(floatingiplist[x].ip + '\n')
+			a = 1
+			break
+	if a == 0:
+
+		floating_ip = nc.floating_ips.create("ext-net")
+
+		server1.add_floating_ip(floating_ip)
+
+		f.write(floating_ip.ip + '\n')
 
 
-#create_proxy(0)
+
+	
+
+	
+
+
+
+
+
+create_proxy(0)
